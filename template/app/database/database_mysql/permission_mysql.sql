ab3ef73b4356c20de7a0bec6d2c7d181b6008e5a
Bjverde <bjverde@yahoo.com.br>
2020-02-14 19:16:42 -0300

    :hammer: #7 incluindo dados

--- template/app/database/database_mysql/permission_mysql.sql
+++ template/app/database/database_mysql/permission_mysql.sql
@@ -207,3 +207,54 @@ DEFAULT CHARACTER SET = utf8;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+INSERT INTO system_group VALUES(1,'Admin');
+INSERT INTO system_group VALUES(2,'Standard');
+
+INSERT INTO system_program VALUES(1,'System Group Form','SystemGroupForm');
+INSERT INTO system_program VALUES(2,'System Group List','SystemGroupList');
+INSERT INTO system_program VALUES(3,'System Program Form','SystemProgramForm');
+INSERT INTO system_program VALUES(4,'System Program List','SystemProgramList');
+INSERT INTO system_program VALUES(5,'System User Form','SystemUserForm');
+INSERT INTO system_program VALUES(6,'System User List','SystemUserList');
+INSERT INTO system_program VALUES(7,'Common Page','CommonPage');
+INSERT INTO system_program VALUES(8,'System PHP Info','SystemPHPInfoView');
+INSERT INTO system_program VALUES(9,'System ChangeLog View','SystemChangeLogView');
+INSERT INTO system_program VALUES(10,'Welcome View','WelcomeView');
+INSERT INTO system_program VALUES(11,'System Sql Log','SystemSqlLogList');
+INSERT INTO system_program VALUES(12,'System Profile View','SystemProfileView');
+INSERT INTO system_program VALUES(13,'System Profile Form','SystemProfileForm');
+INSERT INTO system_program VALUES(14,'System SQL Panel','SystemSQLPanel');
+INSERT INTO system_program VALUES(15,'System Access Log','SystemAccessLogList');
+INSERT INTO system_program VALUES(16,'System Message Form','SystemMessageForm');
+INSERT INTO system_program VALUES(17,'System Message List','SystemMessageList');
+INSERT INTO system_program VALUES(18,'System Message Form View','SystemMessageFormView');
+INSERT INTO system_program VALUES(19,'System Notification List','SystemNotificationList');
+INSERT INTO system_program VALUES(20,'System Notification Form View','SystemNotificationFormView');
+INSERT INTO system_program VALUES(21,'System Document Category List','SystemDocumentCategoryFormList');
+INSERT INTO system_program VALUES(22,'System Document Form','SystemDocumentForm');
+INSERT INTO system_program VALUES(23,'System Document Upload Form','SystemDocumentUploadForm');
+INSERT INTO system_program VALUES(24,'System Document List','SystemDocumentList');
+INSERT INTO system_program VALUES(25,'System Shared Document List','SystemSharedDocumentList');
+INSERT INTO system_program VALUES(26,'System Unit Form','SystemUnitForm');
+INSERT INTO system_program VALUES(27,'System Unit List','SystemUnitList');
+INSERT INTO system_program VALUES(28,'System Access stats','SystemAccessLogStats');
+INSERT INTO system_program VALUES(29,'System Preference form','SystemPreferenceForm');
+INSERT INTO system_program VALUES(30,'System Support form','SystemSupportForm');
+INSERT INTO system_program VALUES(31,'System PHP Error','SystemPHPErrorLogView');
+INSERT INTO system_program VALUES(32,'System Database Browser','SystemDatabaseExplorer');
+INSERT INTO system_program VALUES(33,'System Table List','SystemTableList');
+INSERT INTO system_program VALUES(34,'System Data Browser','SystemDataBrowser');
+INSERT INTO system_program VALUES(35,'System Menu Editor','SystemMenuEditor');
+INSERT INTO system_program VALUES(36,'System Request Log','SystemRequestLogList');
+INSERT INTO system_program VALUES(37,'System Request Log View','SystemRequestLogView');
+INSERT INTO system_program VALUES(38,'System Administration Dashboard','SystemAdministrationDashboard');
+INSERT INTO system_program VALUES(39,'System Log Dashboard','SystemLogDashboard');
+INSERT INTO system_program VALUES(40,'System Session dump','SystemSessionDumpView');
+
+INSERT INTO system_unit VALUES(1,'Unit A','unit_a');
+INSERT INTO system_unit VALUES(2,'Unit B','unit_b');
+
+INSERT INTO system_user VALUES(1,'Administrator','admin','21232f297a57a5a743894a0e4a801fc3','admin@admin.net','Y',10,1);
+INSERT INTO system_user VALUES(2,'User','user','ee11cbb19052e40b07aac0ca060c23ee','user@user.net','Y',7,1);
