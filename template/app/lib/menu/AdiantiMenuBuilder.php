8a2201588b0a715aa00eeb18a18fa3801447350c
Bjverde <bjverde@yahoo.com.br>
2020-04-18 22:18:29 -0300

    :hammer: #8 versão quase funcionando

--- template/app/lib/menu/AdiantiMenuBuilder.php
+++ template/app/lib/menu/AdiantiMenuBuilder.php
@@ -31,11 +31,17 @@ class AdiantiMenuBuilder
                 ob_start();
                 $callback = array('SystemPermission', 'checkPermission');
                 $xml = new SimpleXMLElement(file_get_contents($file));
-                //$menu = new TMenu($xml, $callback,0,'nav navbar-nav','dropdown');
-                $menu = new TMenu($xml, $callback,1,'nav navbar-nav');
-                $menu->id    = 'top-menu';
+                //$menu = new TMenu($xml, $callback,1,'navbar-nav mr-auto','nav-item dropdown','dropdown-item');
+                //$menu = new TMenu($xml, $callback, 1, 'nav-item dropdown');
+                //$menu = new TMenu($xml, $callback, 1, 'nav-item dropdown','dropdown');
+                //$menu = new TMenu($xml, $callback, 1, 'nav-item dropdown','dropdown');
+                $menu = new TMenu($xml, $callback, 1,'dropdown-menu','dropdown','dropdown-toggle');
+                $menu->id    = 'main-menu-top';
                 $menu->show();
                 $menu_string = ob_get_clean();
+                
+                $menu_string = str_replace('class="dropdown-menu level-1" id="main-menu-top"', 'class="nav navbar-nav" id="main-menu-top"', $menu_string);
+                //$menu_string = str_replace('<a href="', '<a class="dropdown-item" href="', $menu_string);
                 return $menu_string;
             break;
             default:
