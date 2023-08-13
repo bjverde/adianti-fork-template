04a8da7b023d8ee1f7a36ba83a6d36e6bb33c786
Bjverde <bjverde@yahoo.com.br>
2023-08-13 00:44:14 -0300

    :hammer: #41 habilitando novo thema

--- template/app/lib/menu/AdiantiMenuBuilder.php
+++ template/app/lib/menu/AdiantiMenuBuilder.php
@@ -59,6 +59,7 @@ class AdiantiMenuBuilder
                 return $menu_string;
                 break;
             case 'theme3':
+            case 'theme3_v5':
                 ob_start();
                 $callback = array('SystemPermission', 'checkPermission');
                 $xml = new SimpleXMLElement(file_get_contents($file));
