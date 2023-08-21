6b1895f9a5f066a3bdd97f2a141e4f2b8ca30bb5
Bjverde <bjverde@yahoo.com.br>
2023-08-21 17:28:10 -0300

    :bug: #45 Fix  Passing null to parameter #2

--- template/lib/adianti/control/TAction.php
+++ template/lib/adianti/control/TAction.php
@@ -253,7 +253,7 @@ class TAction
                 else
                 {
                     $value    = isset($object->$property)? $object->$property : null;
-                    $content  = str_replace($match, $value, $content);
+                    $content  = isset($value)? str_replace($match, $value, $content): null;
                 }
             }
         }
