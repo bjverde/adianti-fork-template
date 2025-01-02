d17f97544e3ed0338bae53fa34d692a2e5855181
Bjverde <bjverde@yahoo.com.br>
2025-01-02 12:02:08 -0300

    :hammer: #57 alterando AdiantiMenuBuilder.php

--- template/app/lib/menu/AdiantiMenuBuilder.php
+++ template/app/lib/menu/AdiantiMenuBuilder.php
@@ -21,6 +21,7 @@ class AdiantiMenuBuilder
         switch ($theme)
         {
             case 'adminbs5':
+            case 'adminbs5_v2':
                 $xml  = new SimpleXMLElement(file_get_contents($file));
                 $menu = new TMenu($xml, self::CHECK_PERMISSION, 1, 'sidebar-dropdown list-unstyled collapse', 'sidebar-item', 'sidebar-link collapsed');
                 $menu->class = 'sidebar-nav';
