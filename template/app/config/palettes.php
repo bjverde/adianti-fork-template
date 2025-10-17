ded6c83ff38e0cb0282eada5986a28a5c71a93f3
Bjverde <bjverde@yahoo.com.br>
2025-10-17 01:40:12 -0300

    🔨 #74 update novos arquivos adianti v8.3.0

---
+++ template/app/config/palettes.php
@@ -0,0 +1,34 @@
+<?php
+return [
+    'palette1' => [
+      '#91afec', '#fcba6d', '#78cd8b', '#f4a1c3', '#c3a643', '#c49ac6', '#e8dc66', '#f37978', '#4791cd', 
+      '#f0abab', '#bfd6ed', '#ffa640', '#ffce9e', '#53b948', '#aadca4', '#ee4b4c', '#ffb6b0', '#a982d3', 
+      '#d3c5de', '#a67a6d', '#d0b5aa', '#ec91d1', '#f9c4dd', '#999999', '#d9d9d9', '#d3cf45', '#e4e4a9', 
+      '#46c9e0', '#afe4f1', '#fba898', '#93e3a3', '#dcd9d6', '#7edabf', '#ffd44f', '#d9ddd9', '#b4dd79', 
+      '#f3bfc9', '#dbd7da', '#c199ff', '#c3d3e5', '#d6f18a', '#f2ecc1', '#dac4aa', '#ffe3cb', '#dac7c4', 
+      '#f0dff0', '#f2f240', '#f0ebdd', '#f6b884', '#bebebe'],
+    'palette2' => [
+      '#33c4b7', '#5f6a6c', '#ff8582', '#f6d84d', '#818d91', '#aadff1', '#feb085', '#bc84ae',
+      '#58a2cd', '#e5d1cf', '#6cd0ca', '#818d7a', '#fca0a0', '#f8e07c', '#a1abac', '#c4e9f4',
+      '#fdb9a8', '#c7a2bb', '#4a93af', '#c2a8a6', '#46a59b', '#4d5b5b', '#cd6a6b', '#d0aa4d',
+      '#6f6f72', '#d4d431', '#e6e6a9', '#45cbe4', '#bceaf3', '#fc9c8b', '#97dd97', '#e0dad6',
+      '#85ddc1', '#ffd84f', '#dde1dd', '#b9dd79', '#f7c3d0', '#e1d7db', '#b399ff', '#c5d5e5',
+      '#def48a', '#f6f0c1', '#dec9aa', '#ffe7cb', '#dec9c4', '#f4e3f4', '#f4f464', '#f4f0ee',
+      '#ffa64d', '#33f047'],
+    'palette3' => [
+      '#99b5d0', '#f88e7b', '#c1dc6e', '#f9df5b', '#78cbaa', '#e3c2a9', '#e69dd5', '#c2c2c2',
+      '#b1dbe1', '#b8eec9', '#349cb4', '#fdcad4', '#6bc35e', '#c9bbda', '#d34c4c', '#32cfd0',
+      '#d9965f', '#8e75b7', '#7ab5f1', '#f1b176', '#d1aea9', '#ec91d1', '#f9c4dd', '#999999',
+      '#d9d9d9', '#d3cf45', '#e4e4a9', '#45cbe4', '#bceaf3', '#fc9c8b', '#97dd97', '#e0dad6',
+      '#85ddc1', '#ffd84f', '#dde1dd', '#b9dd79', '#f7c3d0', '#e1d7db', '#b399ff', '#c5d5e5',
+      '#def48a', '#f6f0c1', '#dec9aa', '#ffe7cb', '#dec9c4', '#f4e3f4', '#f4f464', '#f4f0ee',
+      '#f6c08f', '#bbbbbb'],
+    'palette4' => [
+      '#7a9bbc', '#efa069', '#eb8486', '#9dd3d0', '#7fc27d', '#f6da84', '#ca94bb', '#fbb3b9',
+      '#b18c7e', '#c3baba', '#bfd4ef', '#ffa64f', '#ffd29e', '#53b948', '#aadca4', '#ee4b4c',
+      '#ffb6b0', '#a982d3', '#d3c5de', '#a67a6d', '#d0b5aa', '#ec91d1', '#f9c4dd', '#999999',
+      '#d9d9d9', '#d3cf45', '#e4e4a9', '#45cbe4', '#bceaf3', '#fc9c8b', '#97dd97', '#e0dad6',
+      '#85ddc1', '#ffd84f', '#dde1dd', '#b9dd79', '#f7c3d0', '#e1d7db', '#b399ff', '#c5d5e5',
+      '#def48a', '#f6f0c1', '#dec9aa', '#ffe7cb', '#dec9c4', '#f4e3f4', '#f4f464', '#f4f0ee',
+      '#f6c08f', '#bbbbbb']
+];
